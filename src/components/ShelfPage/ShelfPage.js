6b7e615d4f86658d1e10ae56b701dcea7772468b
James Petran <jm.ptrn@gmail.com>
2022-02-04 10:05:33 -0600

    adding conditional rendering to display form to add item to shelf

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,9 +1,54 @@
 import React from 'react';
+import { useState } from 'react';
 
 function ShelfPage() {
+  const [itemForm, setItemForm] = useState(false)
+  const [descriptionInput, setDescriptionInput] = useState('')
+  const [urlInput, setUrlInput] = useState('')
+
+  const handleSubmit = (evt) => {
+    // prevent reload
+    evt.preventDefault();
+    //do... this
+    console.log('description:', descriptionInput, ' & url:', urlInput);
+    
+    //clear inputs and set item form back to false to remove it from DOM
+    setDescriptionInput('');
+    setUrlInput('');
+    setItemForm(false);
+  }
+
+  const addItem = () => {
+    setItemForm(true);
+  }
+
+
   return (
     <div className="container">
       <h2>Shelf</h2>
+      <div id="item-add" >
+        {itemForm ?
+          <form id="item-form" onSubmit={(evt) => handleSubmit(evt)}>
+            <label htmlFor="item-description">Item Description:</label>
+            <input
+              id="item-description"
+              type="text"
+              value={descriptionInput}
+              onChange={(evt) => setDescriptionInput(evt.currentTarget.value)}
+            />
+            <label htmlFor="item-description">Item URL:</label>
+            <input
+              id="item-url"
+              type="text"
+              value={urlInput}
+              onChange={(evt) => setUrlInput(evt.currentTarget.value)}
+            />
+            <input type="submit"/ >
+          </form>
+          :
+          <button onClick={() => addItem()} > Add Item </button>
+        }
+      </div>
       <p>All of the available items can be seen here.</p>
     </div>
   );
