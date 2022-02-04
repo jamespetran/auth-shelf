5b0bf73438f791ec5417f07205b1dc54da15e7b3
James Petran <jm.ptrn@gmail.com>
2022-02-04 10:59:51 -0600

    cleaned up merge

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
@@ -33,32 +32,6 @@ function ShelfPage() {
     setItemForm(true);
   }
 
-
-=======
-import { useDispatch } from 'react-redux';
-
-function ShelfPage() {
-
-  const dispatch = useDispatch();
-  
-  let testItems = [
-    { id: 1,
-      description: 'a human skull',
-      image_url: 'https://cdn10.bigcommerce.com/s-p1mhyx/products/1773/images/4498/Adult_Human_Skull_-_Asian_Male_Thumbnail__60388.1630008401.1280.1280.jpg?c=2',
-      user_id: 3
-    },
-    { id: 2,
-      description: 'a whoopie cushion',
-      image_url: 'https://m.media-amazon.com/images/I/61IHkpsR9AS._AC_SX425_.jpg',
-      user_id: 2
-    },
-    { id: 3,
-      description: 'a bowl of split pea soup',
-      image_url: 'https://thedeliciouslife.com/wp-content/uploads/2011/12/split-pea-soup.jpg',
-      user_id: 1
-    }
-  ]
-
   const handleDelete = (item) => {
     console.log(`in handleDelete, item to delete is ${item.id}`);
     dispatch({
@@ -67,7 +40,6 @@ function ShelfPage() {
     });
   }
 
->>>>>>> main
   return (
     <div className="container">
       <h2>Shelf</h2>
@@ -88,7 +60,7 @@ function ShelfPage() {
               value={urlInput}
               onChange={(evt) => setUrlInput(evt.currentTarget.value)}
             />
-            <input type="submit"/ >
+            <input type="submit" />
           </form>
           :
           <button onClick={() => addItem()} > Add Item </button>
@@ -96,10 +68,10 @@ function ShelfPage() {
       </div>
       <p>All of the available items can be seen here.</p>
       <ul>
-        { testItems.map( item => (
+        {testItems.map(item => (
           <li key={item.id}>
             <div>
-              <img src={item.image_url} style={{ height: 200, width: 200 }}/>
+              <img src={item.image_url} style={{ height: 200, width: 200 }} />
               {item.description}
               {item.user_id}
               <button onClick={() => handleDelete(item)}>Delete</button>
